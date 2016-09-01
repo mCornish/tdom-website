5f02a891b8819f6d0322deba681a77aeb8f32b0d
Mike Cornish <cornishmw@gmail.com>
2016-09-01 11:01:19 -0400

    Architecture setup

---
+++ public/js/scripts.js
@@ -0,0 +1,15 @@
+'use strict';
+
+$(document).ready(function () {
+  init();
+});
+
+function init() {
+  _bindEvents();
+}
+
+function _bindEvents() {
+  var test = function test() {
+    return console.log('test');
+  };
+}
---
+++ src/js/scripts.js
@@ -0,0 +1,11 @@
+$( document ).ready( function() {
+  init();
+});
+
+function init() {
+  _bindEvents();
+}
+
+function _bindEvents() {
+  
+}
