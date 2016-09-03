3d756aed82247bb8a83e4f23b457b4d6e1980727
Mike Cornish <cornishmw@gmail.com>
2016-09-03 17:23:10 -0400

    Giving up on flashlight for now.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 $(document).ready(function () {
 	init();
@@ -9,12 +9,27 @@ function init() {
 }
 
 function bindEvents() {
-	$(window).on('mousemove', function (e) {
-		var $light = $('.js-flashlight');
-		var width = $light.width();
-		var height = $light.height();
-		var left = e.pageX - width / 2;
-		var top = e.pageY - height / 2;
-		$light.css({ 'margin-left': left + 'px', 'margin-top': top + 'px' });
-	});
+	var BACKGROUND_WIDTH = 3840;
+	var BACKGROUND_HEIGHT = 2160;
+
+	// $( '.js-intro' ).on( 'mousemove', function(e) {
+	// 	const $light = $( '.js-flashlight' );
+	// 	const elWidth = $light.width();
+	// 	const elHeight = $light.height();
+	// 	const winWidth = $( window ).width();
+	// 	const winHeight = $( window ).height();
+	// 	const widthRate = BACKGROUND_WIDTH / winWidth;
+	// 	const heightRate = BACKGROUND_HEIGHT / winHeight;
+
+	// 	const elLeft = e.pageX - ( elWidth / 2 ); // Left position of flashlight
+	// 	const elTop = e.pageY - ( elHeight / 2 ); // Top position of flashlight
+	// 	const backLeft = -( e.pageX - ( elWidth / 2 ) ) * widthRate; // Left position of background
+	// 	const backTop = -( e.pageY - ( elHeight / 2 ) ) * heightRate; // Top position of background
+
+	// 	$light.css({
+	// 		'margin-left': `${elLeft}px`, 
+	// 		'margin-top': `${elTop}px`,
+	// 		'background-position': `${backLeft}px ${backTop}px`
+	// 	});
+	// });
 }
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,18 +1,33 @@
 $( document ).ready( function() {
-  init();
+  	init();
 });
 
 function init() {
-  bindEvents();
+  	bindEvents();
 }
 
 function bindEvents() {
-	$( window ).on( 'mousemove', function(e) {
-		const $light = $( '.js-flashlight' );
-		const width = $light.width();
-		const height = $light.height();
-		const left = e.pageX - ( width / 2 );
-		const top = e.pageY - ( height / 2 );
-		$light.css({'margin-left': `${left}px`, 'margin-top': `${top}px`});
-	});
+	const BACKGROUND_WIDTH = 3840;
+	const BACKGROUND_HEIGHT = 2160;
+
+	// $( '.js-intro' ).on( 'mousemove', function(e) {
+	// 	const $light = $( '.js-flashlight' );
+	// 	const elWidth = $light.width();
+	// 	const elHeight = $light.height();
+	// 	const winWidth = $( window ).width();
+	// 	const winHeight = $( window ).height();
+	// 	const widthRate = BACKGROUND_WIDTH / winWidth;
+	// 	const heightRate = BACKGROUND_HEIGHT / winHeight;
+
+	// 	const elLeft = e.pageX - ( elWidth / 2 ); // Left position of flashlight
+	// 	const elTop = e.pageY - ( elHeight / 2 ); // Top position of flashlight
+	// 	const backLeft = -( e.pageX - ( elWidth / 2 ) ) * widthRate; // Left position of background
+	// 	const backTop = -( e.pageY - ( elHeight / 2 ) ) * heightRate; // Top position of background
+		
+	// 	$light.css({
+	// 		'margin-left': `${elLeft}px`, 
+	// 		'margin-top': `${elTop}px`,
+	// 		'background-position': `${backLeft}px ${backTop}px`
+	// 	});
+	// });
 }
