3e8a2f226e3b68ead750fd3d4790c434571aa2fa
Mike Cornish <cornishmw@gmail.com>
2016-09-03 16:17:43 -0400

    Flashlight progress

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,11 +1,20 @@
-"use strict";
+'use strict';
 
 $(document).ready(function () {
-  init();
+	init();
 });
 
 function init() {
-  _bindEvents();
+	bindEvents();
 }
 
-function _bindEvents() {}
+function bindEvents() {
+	$(window).on('mousemove', function (e) {
+		var $light = $('.js-flashlight');
+		var width = $light.width();
+		var height = $light.height();
+		var left = e.pageX - width / 2;
+		var top = e.pageY - height / 2;
+		$light.css({ 'margin-left': left + 'px', 'margin-top': top + 'px' });
+	});
+}
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,9 +3,16 @@ $( document ).ready( function() {
 });
 
 function init() {
-  _bindEvents();
+  bindEvents();
 }
 
-function _bindEvents() {
-
+function bindEvents() {
+	$( window ).on( 'mousemove', function(e) {
+		const $light = $( '.js-flashlight' );
+		const width = $light.width();
+		const height = $light.height();
+		const left = e.pageX - ( width / 2 );
+		const top = e.pageY - ( height / 2 );
+		$light.css({'margin-left': `${left}px`, 'margin-top': `${top}px`});
+	});
 }
